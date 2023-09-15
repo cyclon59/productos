fe851b00d3c092a7489fd4ee240e7078acea81c1
cyclon59 <camilorivas1405@gmail.com>
2023-09-15 15:10:39 -0300

    prueba1

---
+++ Productos_APP/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    data={"titulo":"hola"}
+    return render(request,'templateAPP1/index.html',data)
+
+
+def producto(request):
+    data={"titulo":"Electronica","imagen":"chancho.jpg",
+          "imagen2":"level 5.png","imagen3":"level 5.png"}
+    return render(request,'templateAPP1/producto.html',data)
+def producto2(request):
+    data={"titulo":"Juguete","imagen":"level 5.png",
+          "imagen2":"chancho.jpg","imagen3":"level 5.png"}
+    return render(request,'templateAPP1/producto.html',data)
+def producto3(request):
+    data={"titulo":"Ropa","imagen":"level 5.png",
+          "imagen2":"level 5.png","imagen3":"chancho.jpg"}
+    return render(request,'templateAPP1/producto.html',data)
+               
+            
